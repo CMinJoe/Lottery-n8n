a6f14fd5680d01426bf91735056a0eb487271b6d
Joe <asd86062377@gmail.com>
2024-10-21 17:05:49 +0800

    add log

--- python-app/app.py
+++ python-app/app.py
@@ -2,6 +2,8 @@ from flask import Flask, jsonify,request
 from TaiwanLottery import TaiwanLotteryCrawler
 # 載入 json 標準函式庫，處理回傳的資料格式
 import json
+import logging
+
 
 # 載入 LINE Message API 相關函式庫
 from linebot import LineBotApi, WebhookHandler
@@ -29,7 +31,7 @@ def linebot():
     body = request.get_data(as_text=True)                    # 取得收到的訊息內容
     try:
         json_data = json.loads(body)   
-        print(json_data)                      # json 格式化訊息內容
+        logging.info("test = %s",jsonify(json_data))                      # json 格式化訊息內容
         access_token = 'DXqQKBJ5xagBiSZmY5Sk/or2SUAjPAMN+FKLSjEXMMzHbh/p/vaWxup/DTZH82ZZLq/q5qxwH6I/FJ9qibDMSD6Ji0Cit/8pYM1Znxfe9/aa7+4VWIz1nlXXFT3/PvOF8dSmxJW8sFyipioXgUONbQdB04t89/1O/w1cDnyilFU='
         secret = 'a33d564adac8b17cca55ab88522898ac'
         line_bot_api = LineBotApi(access_token)              # 確認 token 是否正確
