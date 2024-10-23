f2d9bcf18025c3a4b4d4940d7921d824a837e0a5
Joe <asd86062377@gmail.com>
2024-10-23 11:48:36 +0800

    add Line msg api

--- python-app/app.py
+++ python-app/app.py
@@ -3,6 +3,7 @@ from TaiwanLottery import TaiwanLotteryCrawler
 # 載入 json 標準函式庫，處理回傳的資料格式
 import json
 import logging
+import os
 
 
 # 載入 LINE Message API 相關函式庫
@@ -11,6 +12,9 @@ from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
 
 app = Flask(__name__)
+
+logging.basicConfig(level=logging.INFO)  # 設定日誌級別為 INFO
+logger = logging.getLogger(__name__)
 
 @app.route('/lottery', methods=['GET'])
 def get_data_lottery():
@@ -32,8 +36,8 @@ def linebot():
     try:
         json_data = json.loads(body)   
         logging.info("linebot text = %s",json.dumps(json_data))                      # json 格式化訊息內容
-        access_token = 'DXqQKBJ5xagBiSZmY5Sk/or2SUAjPAMN+FKLSjEXMMzHbh/p/vaWxup/DTZH82ZZLq/q5qxwH6I/FJ9qibDMSD6Ji0Cit/8pYM1Znxfe9/aa7+4VWIz1nlXXFT3/PvOF8dSmxJW8sFyipioXgUONbQdB04t89/1O/w1cDnyilFU='
-        secret = 'a33d564adac8b17cca55ab88522898ac'
+        access_token = os.getenv('LINE_ACCESS_TOKEN')
+        secret = os.getenv('LINE_SECRET')
         line_bot_api = LineBotApi(access_token)              # 確認 token 是否正確
         handler = WebhookHandler(secret)                     # 確認 secret 是否正確
         signature = request.headers['X-Line-Signature']      # 加入回傳的 headers
@@ -51,6 +55,22 @@ def linebot():
     except:
         print(body)                                          # 如果發生錯誤，印出收到的內容
     return 'OK'           
+
+@app.route("/sendMsg", methods=['POST'])
+def linebotSendMsg():
+    body = request.get_data(as_text=True) 
+    print("Request = %s",json.dumps(body));
+    req = json.loads(body);
+    logging.info("Request Json = %s",json.dumps(req))                      # json 格式化訊息內容
+    access_token = os.getenv('LINE_ACCESS_TOKEN');
+    secret = os.getenv('LINE_SECRET');
+    line_bot_api = LineBotApi(access_token)
+    try:
+        # 網址被執行時，等同使用 GET 方法發送 request，觸發 LINE Message API 的 push_message 方法
+        line_bot_api.push_message(secret, TextSendMessage(text=req['sendMsg']))
+        return 'OK'
+    except:
+        print('error')
 
 @app.route('/health')
 def health_check():
